57b8c5be6a131ca7247aef9a9c4f55da5474832b
Jefferson Santos <jheffersonn@hotmail.com>
2023-07-05 01:02:23 -0300

    implementado codigo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n = new Nokia("12133", "abcee", "sdfefeqwe", 64);
+n.InstalarAplicativo($"WhatsApp - Número: {n.Numero} , IMEI: {n.IMEI}");
+n.Ligar();
+n.ReceberLigacao();
+
+Console.WriteLine("--------------------------------------------------");
+
+
+Iphone i = new Iphone("12122", "abcdd", "sdfefeasd", 128);
+i.InstalarAplicativo($"Telegram - Número: {i.Numero} , IMEI: {i.IMEI}");
+i.Ligar();
+i.ReceberLigacao();
